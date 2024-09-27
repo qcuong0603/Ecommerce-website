8cad813ff085d855f720db7084c9586e71a1449a
Bunchaholic <nguyenson27102003@gmail.com>
2024-09-27 11:10:06 +0700

    git commit frontend

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
--- shopping/src/Pages/Shop.jsx
+++ shopping/src/Pages/Shop.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import Hero from '../Components/Hero/hero'
+import Hero  from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollection'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
+
 
 export const Shop = () => {
   return (
     <div>
       <Hero/>
+      <Popular/>
+      <Offers/>
+      <NewCollections/>
+      <NewsLetter/>
     </div>
   )
 }
