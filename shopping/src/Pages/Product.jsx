a45917a19dcd4f36e33618bd825a5198e0c2985b
Bunchaholic <nguyenson27102003@gmail.com>
2024-09-28 09:22:10 +0700

    git commit breadcrum

--- shopping/src/Pages/Product.jsx
+++ shopping/src/Pages/Product.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../Components/Context/ShopContext';
+import {Breadcrum} from '../Components/Breadcrums/Breadcrum';
 
 export const Product = () => {
+  const {all_product} = useContext(ShopContext)
+  const {productId} = useParams();
+  const product = all_product.find((e)=>e.id === Number(productId))
+
   return (
     <div>
-
+      <Breadcrum product={product}/>
     </div>
   )
 }
