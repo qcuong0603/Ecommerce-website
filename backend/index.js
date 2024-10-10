9b9d9da54ea1de5876225bc113223ec6de1b8156
Bunchaholic <nguyenson27102003@gmail.com>
2024-10-10 16:22:05 +0700

    git commit update

--- backend/index.js
+++ backend/index.js
@@ -19,14 +19,6 @@ app.get("/",(req,res)=>{
 })
 
 
-//Image Storage Engine
-
-const storage = multer.diskStorage({
-    destination: './upload/images',
-    filename:(req,file,cb)=>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.origianlname)}`)
-    }
-})
 
 const upload = multer({storage:storage})
 
@@ -35,12 +27,7 @@ const upload = multer({storage:storage})
 app.use('/images',express.static('upload/images'))
 
 
-app.post("/upload",upload.single('product'),(req,res)=>{
-    res.json({
-        success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
-    })
-})
+
 
 // Schema for creating products
 
@@ -82,6 +69,7 @@ app.post('/addproduct',async (req,res)=>{
     const product = new Product({
         id:req.body.id,
         name:req.body.name,
+        image:req.body.image,
         category:req.body.category,
         new_price:req.body.new_price,
         old_price:req.body.old_price,
